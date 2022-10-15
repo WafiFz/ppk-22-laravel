2e763148fc63355ed10c7f701989676a5d64e7a3
WafiFz <wafi20002@mail.unpad.ac.id>
2022-10-16 06:21:09 +0700

    apply auth

--- pupuk/routes/api.php
+++ pupuk/routes/api.php
@@ -26,37 +26,37 @@ Route::post('/admin/login', [AdminController::class, 'login']);
 //Pupuk - without auth
 Route::post('/v2/pupuk', [PupukController::class, 'create']);
 
-// Pupuk   
-Route::get('/v2/pupuk', [PupukController::class, 'view']);
-Route::get('/v2/pupuk', [PupukController::class, 'view']);
-Route::get('/v2/pupuk/{id}', [PupukController::class, 'viewByID']);
-Route::put('/v2/pupuk/{id}', [PupukController::class, 'updateByID']);
-Route::delete('/v2/pupuk/{id}', [PupukController::class, 'deleteByID']);
+// // Pupuk   
+// Route::get('/v2/pupuk', [PupukController::class, 'view']);
+// Route::get('/v2/pupuk', [PupukController::class, 'view']);
+// Route::get('/v2/pupuk/{id}', [PupukController::class, 'viewByID']);
+// Route::put('/v2/pupuk/{id}', [PupukController::class, 'updateByID']);
+// Route::delete('/v2/pupuk/{id}', [PupukController::class, 'deleteByID']);
 
-// Sampah
-Route::post('/v2/sampah', [SampahController::class, 'create']);
-Route::get('/v2/sampah', [SampahController::class, 'view']);
-Route::get('/v2/sampah', [SampahController::class, 'view']);
-Route::get('/v2/sampah/{id}', [SampahController::class, 'viewByID']);
-Route::put('/v2/sampah/{id}', [SampahController::class, 'updateByID']);
-Route::delete('/v2/sampah/{id}', [SampahController::class, 'deleteByID']);
+// // Sampah
+// Route::post('/v2/sampah', [SampahController::class, 'create']);
+// Route::get('/v2/sampah', [SampahController::class, 'view']);
+// Route::get('/v2/sampah', [SampahController::class, 'view']);
+// Route::get('/v2/sampah/{id}', [SampahController::class, 'viewByID']);
+// Route::put('/v2/sampah/{id}', [SampahController::class, 'updateByID']);
+// Route::delete('/v2/sampah/{id}', [SampahController::class, 'deleteByID']);
 
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::post('/logout', [UserController::class, 'logout']);
-//     Route::post('/admin/logout', [AdminController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/admin/logout', [AdminController::class, 'logout']);
 
-//     // Pupuk   
-//     Route::get('/v2/pupuk', [PupukController::class, 'view']);
-//     Route::get('/v2/pupuk', [PupukController::class, 'view']);
-//     Route::get('/v2/pupuk/{id}', [PupukController::class, 'viewByID']);
-//     Route::put('/v2/pupuk/{id}', [PupukController::class, 'updateByID']);
-//     Route::delete('/v2/pupuk/{id}', [PupukController::class, 'deleteByID']);
+    // Pupuk   
+    Route::get('/v2/pupuk', [PupukController::class, 'view']);
+    Route::get('/v2/pupuk', [PupukController::class, 'view']);
+    Route::get('/v2/pupuk/{id}', [PupukController::class, 'viewByID']);
+    Route::put('/v2/pupuk/{id}', [PupukController::class, 'updateByID']);
+    Route::delete('/v2/pupuk/{id}', [PupukController::class, 'deleteByID']);
 
-//     // Sampah
-//     Route::post('/v2/sampah', [SampahController::class, 'create']);
-//     Route::get('/v2/sampah', [SampahController::class, 'view']);
-//     Route::get('/v2/sampah', [SampahController::class, 'view']);
-//     Route::get('/v2/sampah/{id}', [SampahController::class, 'viewByID']);
-//     Route::put('/v2/sampah/{id}', [SampahController::class, 'updateByID']);
-//     Route::delete('/v2/sampah/{id}', [SampahController::class, 'deleteByID']);
-// });
+    // Sampah
+    Route::post('/v2/sampah', [SampahController::class, 'create']);
+    Route::get('/v2/sampah', [SampahController::class, 'view']);
+    Route::get('/v2/sampah', [SampahController::class, 'view']);
+    Route::get('/v2/sampah/{id}', [SampahController::class, 'viewByID']);
+    Route::put('/v2/sampah/{id}', [SampahController::class, 'updateByID']);
+    Route::delete('/v2/sampah/{id}', [SampahController::class, 'deleteByID']);
+});
